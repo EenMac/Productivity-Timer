912535ddc68d2c65bf74b1c55c63d7b1a0265aa7
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-04-07 16:54:37 +0100

    Time and lap funcionality added

--- src/container/timeContainer.js
+++ src/container/timeContainer.js
@@ -5,13 +5,15 @@ import OffList from '../components/OffList'
 import OnList from '../components/OnList'
 import OffListItem from '../components/OffListItem'
 import OnListItem from '../components/OnListItem'
+import { renderIntoDocument } from 'react-dom/test-utils'
 
 
 
 function TimeContainer() {
   const [timer, setTimer] = useState(0)
   const [active, setActive] = useState(false)
-  const [laps, setLaps] = useState([])
+  const [pause, setPause] = useState(false)
+  const [lap, setLap] = useState([])
 
   useEffect(() => {
   let interval;
@@ -25,11 +27,17 @@ function TimeContainer() {
   return () => clearInterval(interval);
 }, [active])
   
-  const handleLaps = (laps) => {
-    setLaps(() => laps)
-    return
+  const handleStart = () => {
+    setActive(true)
   }
 
+  const handleStop = () => {
+    setActive(false)
+  }
+
+  const handleLap = () => {
+    setLap((lap)=> [...lap, {timer}]);
+  }
 
 
   const formatTime = () => {
@@ -40,22 +48,30 @@ function TimeContainer() {
    return `${minutes}:${seconds}:${milliSeconds}`
   }
 
-  
+  function laps(formatTime){
+    document.getElementById("list-items").innerHTML = formatTime
+  }
 
+  const handleLaps = (time) => {
+    let lap = document.getElementById("timer").text
+  }
+
+  
   return (
     <div className="stopwatch">
       <div className="numbers">
-        <h1>{formatTime()}</h1>
+        <h1 id="timer">{formatTime()}</h1>
       </div>
       <div className="buttons">
-        <button onClick={() => setActive(true)}>Start</button>
-        <button onClick={() => setActive(false)}>Stop</button>
-        <button onClick={() => setTimer(0)}>Reset</button>       
+        <button onClick={() => handleStart()}>Start</button>
+        <button onClick={() => handleStop()}>Stop</button>
+        <button onClick={() => setTimer(0)}>Reset</button>
+        <button onClick={handleLaps}>Lap</button>       
       </div>
       <div>
         <ul>
           <li>
-            <p id="list-items"></p>
+            <p id="list-items">{formatTime()}</p>
           </li>
         </ul>
       </div>
